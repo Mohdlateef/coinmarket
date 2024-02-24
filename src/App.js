ec56c66a42b641eefcc5d50d88b85d2a75f09bfe
unknown <lateefahbaba123@gmail.com>
2024-02-24 14:28:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Getcoins from "./Components/getcoins";
+import './App.css'
+const App=()=>{
+
+
+
+  return(
+    <div>
+      <Getcoins />
+
+    </div>
+  )
+}
+export default App
---
+++ src/Components/getcoins.js
@@ -0,0 +1,44 @@
+import React,{useState,useEffect} from "react";
+
+const Getcoins=()=>{
+    let [Data,SetData]=useState("");
+    
+useEffect(()=>{
+getdata();
+console.log("data");
+},[])
+
+
+async function getdata(){
+
+
+
+  try{  let response=await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false")
+    let data=await response.json();
+    console.log(data);
+    SetData(data);
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+// getdata();
+
+    return (
+        <table className="table">{
+    Data&&(Data.map((iteam,index)=>(
+        <tr key={index} >
+            <td><img src={iteam.image} height={"35px"}></img></td>
+            <td>{iteam.name}</td>
+        <td>{iteam.symbol}</td>
+        <td>{iteam.current_price}</td>
+        <td>{iteam.circulating_supply}</td>
+        <td>{iteam.ath_change_percentage}</td>
+        <td>Mkt Cap:{iteam.market_cap}</td>
+        </tr>
+    )))
+}
+        </table>
+    )
+}
+export default Getcoins;
